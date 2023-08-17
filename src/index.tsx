7c0b47da7eaedc23a968c80968cc15c44d2107d1
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-08-17 19:26:57 +0300

    fix: refactoring index

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './app/App'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
-import {
-    BrowserRouter,
-    createBrowserRouter, createHashRouter,
-    RouterProvider,
-} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {store} from './app/store'
+import {createHashRouter, RouterProvider,} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -20,6 +16,3 @@ root.render(
     </Provider>,
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
